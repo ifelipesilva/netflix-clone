a88c8e1761942d5a393e69069a3cc2d1da2f2ce7
ifelipesilva <feehlipeeh422@gmail.com>
2022-01-05 14:38:32 -0300

    first commit

---
+++ src/home/Home.jsx
@@ -0,0 +1,12 @@
+import { AcUnit } from '@material-ui/icons';
+import './home.scss';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>oi</h1>
+    </div>
+  );
+};
+
+export default Home;
