cffd523fbc1561922a1c7dc082a9c8a8c8bd8d25
ifelipesilva <feehlipeeh422@gmail.com>
2022-01-05 16:43:01 -0300

    slider

---
+++ src/components/listItem/ListItem.jsx
@@ -0,0 +1,30 @@
+import './listItem.scss';
+import {
+  PlayArrow,
+  Add,
+  ThumbUpAltOutlined,
+  ThumbDownOutlined,
+} from '@material-ui/icons';
+import { useState } from 'react';
+
+const ListItem = () => {
+  return (
+    <div className="listItem">
+      <img
+        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+        alt=""
+      />
+
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayArrow />
+          <Add />
+          <ThumbUpAltOutlined />
+          <ThumbDownOutlined />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListItem;
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,4 +1,5 @@
 import Featured from '../components/featured/Featured';
+import List from '../components/list/List';
 import Navbar from '../components/navbar/Navbar';
 import './home.scss';
 
@@ -6,8 +7,11 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
-
       <Featured />
+      <List />
+      <List />
+      <List />
+      <List />
     </div>
   );
 };
