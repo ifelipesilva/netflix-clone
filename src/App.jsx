f22e19efb61b23fad429d1d9f274e995fc6b7c3b
ifelipesilva <feehlipeeh422@gmail.com>
2022-01-05 17:51:55 -0300

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import Watch from './pages/watch/Watch';
 import './app.scss';
 import Home from './pages/home/Home';
 import Register from './pages/register/Register';
+import Login from './pages/login/Login';
 
 const App = () => {
-  return <Register />;
+  return <Login />;
 };
 
 export default App;
